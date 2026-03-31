553b5896af6c13cd8f704b7314b8fe651950142a
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2026-03-31 18:59:29 -0300

    Merging
    Merging programing house with addVariableMoney

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const incomeVariables = []
 await entraces.entraceFixedIncome(painelFixedIncome,'Transoporte', 300)
 await entraces.entraceFixedIncome(painelFixedIncome, 'Sario', 1200)
 
-await incomes.variableMoney(incomeVariables, 30, 'Copa do Mundo')
-await incomes.variableMoney(incomeVariables, 70, 'Celular')
+await entraces.variableMoney(incomeVariables, 30, 'Copa do Mundo')
+await entraces.variableMoney(incomeVariables, 70, 'Celular')
 
 await functions.viewIncomeVariable(incomeVariables)
 
